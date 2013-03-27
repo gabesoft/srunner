f9a44a4c181578be0667329885084266da034464
Gabriel Adomnicai <gabesoft@groupon.com>
2013-03-27 15:15:38 -0700

    fixes

--- scripts/ensure_dir_exists.js
+++ scripts/ensure_dir_exists.js
@@ -1,11 +1,15 @@
-fs   = require 'fs'
-path = require 'path'
+var fs   = require('fs')
+  , path = require('path');
 
-module.exports = (state, dir, cb) ->
-  dir = path.resolve dir
-  fs.stat dir, (err, stat) ->
-    if err? or not stat.isDirectory()
-      fs.mkdir dir, cb
-    else
-      cb()
+module.exports = function (state, dir, cb) {
+    dir = path.resolve(dir);
+
+    fs.stat(dir, function (err, stat) {
+        if (err || !stat.isDirectory()) {
+            fs.mkdir(dir, cb);
+        } else {
+            cb();
+        }
+    });
+};
 
