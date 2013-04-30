42adbc3fe11f62b011e1ef650d4b6f2411f62a7c
Gabriel Adomnicai <gabesoft@gmail.com>
2013-04-29 21:39:34 -0700

    logging fixes

--- scripts/ensure_dir_exists.js
+++ scripts/ensure_dir_exists.js
@@ -4,22 +4,24 @@ var fs     = require('fs')
   , path   = require('path');
 
 module.exports = function (state, dir, cb) {
-    level = core.isString(dir) ? 'info' : (dir.quiet ? 'debug' : 'info');
-    dir   = core.isString(dir) ? dir : (dir.dir || dir.path);
-    dir   = path.resolve(dir);
-    log   = state.log;
+    levelCreated = core.isString(dir) ? 'info' : (dir.quiet ? 'debug' : 'info');
+    levelExists  = core.isString(dir) ? 'warn' : (dir.quiet ? 'debug' : 'warn');
+
+    dir = core.isString(dir) ? dir : (dir.dir || dir.path);
+    dir = path.resolve(dir);
+    log = state.log;
 
     fs.stat(dir, function (err, stat) {
         if (err || !stat.isDirectory()) {
             mkdirp(dir, function (err) {
-                if (!err && log && log[level]) {
-                    log[level]('directory ' + dir.green + ' created');
+                if (!err && log && log[levelCreated]) {
+                    log[levelCreated]('directory ' + dir.green + ' created');
                 }
                 cb(err);
             });
         } else {
-            if (log && log[level]) {
-                log[level]('directory ' + dir.blue + ' already exists');
+            if (log && log[levelExists]) {
+                log[levelExists]('directory ' + dir.blue + ' already exists');
             }
             cb();
         }
